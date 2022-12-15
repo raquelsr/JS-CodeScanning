c1c030a5ee6327867107ee38ece995905e5a1d14
Raquel Serrano <raquelserranor@hotmail.com>
2022-12-15 13:09:28 +0100

    add js file with errors

---
+++ index.js
@@ -0,0 +1,10 @@
+const password = '123Password';
+alert('Alert! Alert!')
+
+password = 'Anotherpassword';
+
+while (true) {
+    console.log(password)
+}
+
+console.log('line 6');
--- raquel.js
+++
@@ -1,8 +0,0 @@
-const password = '123Password';
-alert('Raquel')
-
-// while (true) {
-//     console.log(password)
-// }
-
-// console.log('line 6');
